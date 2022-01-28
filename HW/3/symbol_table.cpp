da2a035010b0207133633dcb527abf91133b9cf9
Dolev Shema <shemadolev@gmail.com>
2022-01-28 18:47:53 +0200

    store/load ids: CITOF only if needed

--- HW/3/symbol_table.cpp
+++ HW/3/symbol_table.cpp
@@ -45,12 +45,12 @@ void
 TypedVarScopeTable::emitStoreIds(){
     int regs_num = _curTempOffset - _tempStartingIndex;
     string regSP = SP;
-    if(_type == eFLOAT){
-        //Cast SP to a SP_F
-        code.emit("CITOF",SP_F, SP);
-        regSP = SP_F;
-    }
     if(regs_num > 0){
+        if(_type == eFLOAT){
+            //Cast SP to a SP_F
+            code.emit("CITOF",SP_F, SP);
+            regSP = SP_F;
+        }
         //Store registers
         for (int i=0; i<regs_num; i++){
             code.emit(string("STOR")+_typeLetter + " " +  _typeLetter + to_string(_tempStartingIndex + i) + " " + regSP + " " + to_string(i * VAR_SIZE));
@@ -66,14 +66,12 @@ TypedVarScopeTable::emitLoadIds(){
     if(regs_num > 0){
         //Update SP
         code.emit(string("SUBTI ") + SP + " " + SP + " " + to_string(regs_num * VAR_SIZE));
-    }    
-    string regSP = SP;
-    if(_type == eFLOAT){
-        //Cast SP to a SP_F
-        code.emit("CITOF",SP_F, SP);
-        regSP = SP_F;
-    }
-    if(regs_num > 0){
+        string regSP = SP;
+        if(_type == eFLOAT){
+            //Cast SP to a SP_F
+            code.emit("CITOF",SP_F, SP);
+            regSP = SP_F;
+        }
         //Store registers
         for (int i=0; i<regs_num; i++){
             code.emit(string("LOAD")+_typeLetter + " " +  _typeLetter + to_string(_tempStartingIndex + i) + " " + regSP + " " + to_string(i * VAR_SIZE));
